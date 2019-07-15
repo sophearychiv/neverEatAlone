8c92e74d25b9f1ea2d730907db2b5590e1ed135d
Sophearychiv <sopheary.chiv@gmail.com>
2019-07-15 16:22:29 -0700

    improved search rest page

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, TextInput} from 'react-native';
+import {Text, View, TextInput, StyleSheet} from 'react-native';
 import { Container, Header, Content, Form, Item, Input, Label, Button } from 'native-base';
 
 class SearchBar extends React.Component {
@@ -25,11 +25,8 @@ class SearchBar extends React.Component {
 
     render() {
         return (
+
             <Container>
-                <View style={styles.searchTextSection}>
-                    <Header>
-                        <Text style={styles.header}>Search Restaurants</Text>
-                    </Header>
                     <Content>
                         <Form>
                             <Item fixedLabel>
@@ -48,43 +45,43 @@ class SearchBar extends React.Component {
                                     onChangeText={val => this.updateCategoryState(val)}
                                 />
                             </Item>
+                            <Button
+                                primary
+                                style={styles.submitButton}
+                                onPress={() => this.props.updateSearch(this.state.location, this.state.category)}
+                            >
+                                <Text style={styles.submitText}> Submit </Text>
+                            </Button>
                         </Form>
                     </Content>
-                </View>
-                <View style={styles.submitButton}>
-                        <Button 
-                            primary
-                            onPress={() => this.props.updateSearch(this.state.location, this.state.category)}
-                        >
-                            <Text style={styles.submitText}> Submit </Text>
-                        </Button>
-                </View>
             </Container>
-            
-                
         );
     }
 }
 
-const styles = {
-    header: {
-        color: "white",
-        textAlignVertical: "center"
-    },
+
+const styles = StyleSheet.create({
+    
     searchTextSection: {
         width: '100%',
         height: '30%'
     },
     submitText: {
         color: "white",
-        textAlign: "center"
+        fontSize: 20,
+        // textAlignVertical: "center"
     },
+
     submitButton: {
-        width: '100%',
-        height: '30%',
         justifyContent: "center",
-        // alignItems: "center"
+        // flex: 0,
+        marginTop: "5%",
+        width: "60%",
+        alignSelf: "center",
+        // alignSelf: "stretch",
+        borderRadius: 5,
+        textAlignVertical: "center"
     }
-}
+});
 
 export default SearchBar;
