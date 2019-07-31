217f47f16fc7b80a3c0fb9329d34faf238a3cfcf
Sophearychiv <sopheary.chiv@gmail.com>
2019-07-31 10:35:00 -0700

    removed comments

--- components/FooterTabs.js
+++ components/FooterTabs.js
@@ -10,11 +10,6 @@ class FooterTabs extends Component {
         }
     }
     
-    // componentDidUpdate(prevState){
-    //     if(this.state.currentTab != prevState.currentTab){
-            
-    //     }
-    // }
     clickOnHome = () => {
 
         this.setState({
@@ -22,15 +17,6 @@ class FooterTabs extends Component {
         });
         this.props.navigation.navigate("Home");
     }
-
-    // clickOnSearch = () => {
-    //     this.setState({
-    //         currentTab: "search"
-    //     });
-    //     this.props.navigation.navigate("SearchRestaurants", {
-    //         fbId: this.props.fbId
-    //     });
-    // }
 
     clickOnInvites = () => {
         this.props.navigation.navigate("Invites", {
@@ -58,55 +44,6 @@ class FooterTabs extends Component {
     }
     render() {
 
-        // let displayTabs;
-        // if (this.state.currentTab === "home") {
-
-        //     displayTabs = <FooterTab>
-        //         <Button vertical active onPress={() => this.clickOnHome()}>
-        //             <Icon name="home" />
-        //             <Text>Home</Text>
-        //         </Button>
-
-        //         <Button vertical onPress={() => this.clickOnSearch()}>
-        //             <Icon name="search" />
-        //             <Text>Search</Text>
-        //         </Button>
-
-        //         <Button vertical onPress={() => this.clickOnInvites()}>
-        //             <Icon name="mail-open" />
-        //             <Text>Invites</Text>
-        //         </Button>
-
-        //         <Button vertical onPress={() => this.clickOnFav()}>
-        //             <Icon name="heart" />
-        //             <Text>Fav</Text>
-        //         </Button>
-        //     </FooterTab>
-        // }
-        
-        // if (this.state.currentTab === "search"){
-        //     displayTabs = <FooterTab>
-        //     <Button vertical onPress={() => this.clickOnHome()}>
-        //         <Icon name="home" />
-        //         <Text>Home</Text>
-        //     </Button>
-
-        //     <Button vertical active onPress={() => this.clickOnSearch()}>
-        //         <Icon name="search" />
-        //         <Text>Search</Text>
-        //     </Button>
-
-        //     <Button vertical onPress={() => this.clickOnInvites()}>
-        //         <Icon name="mail-open" />
-        //         <Text>Invites</Text>
-        //     </Button>
-
-        //     <Button vertical onPress={() => this.clickOnFav()}>
-        //         <Icon name="heart" />
-        //         <Text>Fav</Text>
-        //     </Button>
-        // </FooterTab>
-        // }
 
         const home = this.state.currentTab === "home" ?
             <Button vertical onPress={() => this.clickOnHome()}>
@@ -119,19 +56,8 @@ class FooterTabs extends Component {
                 <Text>Home</Text>
             </Button>
 
-        // const search = this.state.currentTab === "search" ?
-        //     <Button vertical onPress={() => this.clickOnSearch()}>
-        //     {/* <Button vertical active onPress={() => this.clickOnSearch()}> */}
-        //         <Icon name="search" />
-        //         <Text>Search</Text>
-        //     </Button>
-        //     : <Button vertical onPress={() => this.clickOnSearch()}>
-        //         <Icon name="search" />
-        //         <Text>Search</Text>
-        //     </Button>
         const invites = this.state.currentTab === "invites" ?
             <Button vertical onPress={() => this.clickOnInvites()}>
-            {/* <Button vertical active onPress={() => this.clickOnInvites()}> */}
                 <Icon name="mail-open" />
                 <Text>Invites</Text>
             </Button>
