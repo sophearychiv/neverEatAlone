4571f2e26b3d31ff6a2a92e9a2a6e320519572ee
Sophearychiv <sopheary.chiv@gmail.com>
2019-07-31 10:52:12 -0700

    added getPendingInvites and getReadPendingInvites in FooterTabs

--- components/FooterTabs.js
+++ components/FooterTabs.js
@@ -3,13 +3,93 @@ import { withNavigation } from 'react-navigation';
 import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
 class FooterTabs extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            currentTab: "home"
+            currentTab: "home",
+            fbId: null,
+            isLoggedIn: null,
+            name: null,
+            photoUrl: null
         }
+        this.getToken();
     }
-    
+
+    async getToken() {
+
+        await AsyncStorage.getItem('access_token')
+            .then(response => {
+                const userInfo = JSON.parse(response);
+                this.setState({
+                    fbId: userInfo.id,
+                    isLoggedIn: true,
+                    name: userInfo.name,
+                    photoUrl: userInfo.picture.data.url
+                });
+                return userInfo;
+            })
+            .then(userInfo => {
+                this.getPendingInvites(userInfo);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    getPendingInvites = async (userInfo) => {
+        const IN_USE_HTTP = require("../internet.json").IN_USE_HTTP
+        axios.get(IN_USE_HTTP + "/users/" + userInfo.id + "/invites/received")
+            .then(response => {
+                this.setState({
+                    pendingInvites: response.data.data,
+                });
+                console.log("pending invites in FooterTabs: " + JSON.stringify(response.data.data));
+
+                return response.data.data
+            })
+            .then(pendingInvites => {
+                console.log("fbId in FooterTab: " + userInfo.fbId);
+                console.log("fbId in state in FooterTab: " + this.state.fbId);
+
+                axios.get(IN_USE_HTTP + "/users/" + this.state.fbId + "/invites/read")
+                    .then(response => {
+                        console.log("read invites in FooterTabs: " + JSON.stringify(response));
+                        if (response.data.data.length < pendingInvites.length) {
+                            this.setState({
+                                badgeCount: pendingInvites.length - response.data.data.length,
+                                readInvites: response.data.data
+                            })
+                        }
+                    })
+                    .catch(error => {
+                        console.log("error getting read invites in FooterTabs: " + error);
+                    })
+            })
+
+            .catch(error => {
+                console.log("error requesting invites in FooterTabs component: " + error);
+            })
+    }
+
+    getReadInvites = async () => {
+        console.log("fbId in FooterTabs: " + this.props.fbId);
+        axios.get(IN_USE_HTTP + "/users/" + this.props.fbId + "/invites/read")
+            .then(response => {
+                console.log("read invites in FooterTabs: " + JSON.stringify(response.data.data));
+                console.log("pending invites in FooterTabs: " + JSON.stringify(this.props.pendingInvites));
+                if (response.data.data.length < this.props.pendingInvites.length) {
+                    this.setState({
+                        //pendingInvites is from Home
+                        badgeCount: this.props.pendingInvites.length - response.data.data.length,
+                        readInvites: response.data.data
+                    })
+                }
+            })
+            .catch(error => {
+                console.log("error getting read invites in FooterTabs: " + error);
+            })
+    }
+
     clickOnHome = () => {
 
         this.setState({
@@ -47,7 +127,7 @@ class FooterTabs extends Component {
 
         const home = this.state.currentTab === "home" ?
             <Button vertical onPress={() => this.clickOnHome()}>
-            {/* <Button vertical active onPress={() => this.clickOnHome()}> */}
+                {/* <Button vertical active onPress={() => this.clickOnHome()}> */}
                 <Icon name="home" />
                 <Text>Home</Text>
             </Button>
@@ -68,7 +148,7 @@ class FooterTabs extends Component {
 
         const fav = this.state.currentTab === "fav" ?
             <Button vertical onPress={() => this.clickOnFav()}>
-            {/* <Button vertical active onPress={() => this.clickOnFav()}> */}
+                {/* <Button vertical active onPress={() => this.clickOnFav()}> */}
                 <Icon name="heart" />
                 <Text>Fav</Text>
             </Button>
